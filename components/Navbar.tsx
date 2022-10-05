449f654279dd82926c916983fb2a21e9f6d6670e
Andrew <andrewmorrison10@sky.com>
2022-10-05 18:01:19 +0100

    Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, Highlight, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure, useColorMode } from '@chakra-ui/react'
+import { Box, Button, Heading, Highlight, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useColorMode, useColorModeValue, useDisclosure } from '@chakra-ui/react'
 import { SunIcon } from '@chakra-ui/icons'
 import { auth, googleAuthProvider } from '../lib/firebase'
 import { signInWithPopup, signOut } from 'firebase/auth'
@@ -6,14 +6,13 @@ import { useContext } from 'react'
 import { UserContext } from '@lib/context'
 import { useRouter } from 'next/router'
 import UsernameForm from './UsernameForm'
-import { useColorModeValue } from '@chakra-ui/react'
 
 export default function Navbar() {
   const { user, username } = useContext(UserContext)
   const { toggleColorMode } = useColorMode()
   const { isOpen, onOpen, onClose } = useDisclosure()
   const highlightBG = useColorModeValue('#000', '#FFF')
-  const highlighTxt = useColorModeValue('#FFF','#000')
+  const highlighTxt = useColorModeValue('#FFF', '#000')
   const router = useRouter()
 
   const signOutNow = () => {
@@ -32,11 +31,10 @@ export default function Navbar() {
       <Heading fontSize="40px" lineHeight="tall">
         <Highlight
           query="News"
-          styles={{ 
-            rounded: 'full',
+          styles={{ rounded: 'full',
             px: '2',
             color: highlighTxt,
-            bg: highlightBG}}
+            bg: highlightBG }}
         >
           NxtNews
         </Highlight>
@@ -44,27 +42,27 @@ export default function Navbar() {
           Personalised news platform giving you the stories that matter.
         </Text>
       </Heading>
-      <Button onClick={toggleColorMode} marginLeft='auto'>
-        <SunIcon/>
+      <Button marginLeft="auto" onClick={toggleColorMode}>
+        <SunIcon />
       </Button>
-      {username && (
-        <>
-          <Button onClick={signOutNow}>Sign Out</Button>
-        </>
-      )}
+      {username
+        ? <Button onClick={signOutNow}>Sign Out</Button>
+        : null}
 
       {!username &&
-        <Button ml='5' onClick={onOpen}>Sign In</Button>}
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay/>
-          <ModalContent>
-            <ModalHeader>Sign In</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              {user ? <UsernameForm/> : <SignInButton />}
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+        <Button ml="5" onClick={onOpen}>Sign In</Button>}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Sign In</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {user
+              ? <UsernameForm />
+              : <SignInButton />}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   )
 }
--- components/UsernameForm.tsx
+++ components/UsernameForm.tsx
@@ -3,16 +3,22 @@ import { UserContext } from '@lib/context'
 import { Field, Form, Formik } from 'formik'
 import React, { useCallback, useContext, useEffect, useState } from 'react'
 import debounce from 'lodash/debounce'
-import { doc, getDoc, getFirestore, writeBatch } from 'firebase/firestore';
+import { doc, getDoc, getFirestore, writeBatch } from 'firebase/firestore'
 
 export default function UsernameForm(): JSX.Element {
-  const [formValue, setFormValue] = useState('')
-  const [isValid, setIsValid] = useState(false)
-  const { user, username } = useContext(UserContext);
+  const [
+    formValue,
+    setFormValue
+  ] = useState('')
+  const [
+    isValid,
+    setIsValid
+  ] = useState(false)
+  const { user, username } = useContext(UserContext)
   const { onClose } = useModalContext()
 
   useEffect(() => {
-    if(user && username) {
+    if (user && username) {
       onClose()
     }
   })
@@ -20,78 +26,85 @@ export default function UsernameForm(): JSX.Element {
   const onValidate = (name: string) => {
     let error
     const val = name.toLowerCase()
-  
+
     if (!val) {
       return 'Name is required'
     }
 
     setFormValue(name)
     if (!isValid) {
-      error ='Name is taken'
+      error = 'Name is taken'
     }
     // Only set form value if length is < 3 OR it passes regex
     if (val.length < 3) {
       error = 'Name is too short'
-      }
-    return error
-  
-  }
-  
-  useEffect(() => {
-    checkUsername(formValue);
-  }, [formValue]);
+    }
 
-  // Hit the database for username match after each debounced change
-  // useCallback is required for debounce to work
+    return error
+
+  }
+
+  useEffect(() => {
+    checkUsername(formValue)
+  }, [formValue])
+
+  /*
+   * Hit the database for username match after each debounced change
+   * useCallback is required for debounce to work
+   */
   const checkUsername = useCallback(
     debounce(async (username:string) => {
       if (username.length >= 3) {
-        const ref = doc(getFirestore(), 'usernames', username);
-        const snap = await getDoc(ref);
-        console.log('Firestore read executed!', snap.exists());
-        setIsValid(!snap.exists());
+        const ref = doc(getFirestore(), 'usernames', username)
+        const snap = await getDoc(ref)
+
+        console.log('Firestore read executed!', snap.exists())
+        setIsValid(!snap.exists())
       }
     }, 500),
     []
-  );
+  )
 
   const onSubmit = async () => {
 
     // Create refs for both documents
-    const userDoc = doc(getFirestore(), 'users', user.uid);
-    const usernameDoc = doc(getFirestore(), 'usernames', formValue);
+    const userDoc = doc(getFirestore(), 'users', user.uid)
+    const usernameDoc = doc(getFirestore(), 'usernames', formValue)
 
     // Commit both docs together as a batch write.
-    const batch = writeBatch(getFirestore());
-    batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName });
-    batch.set(usernameDoc, { uid: user.uid });
+    const batch = writeBatch(getFirestore())
 
-    await batch.commit();
-  };
+    batch.set(userDoc, { username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName })
+    batch.set(usernameDoc, { uid: user.uid })
+
+    await batch.commit()
+  }
+
   return (
     <Formik
-      initialValues={{ name: 'test'}}
+      initialValues={{ name: 'test' }}
       onSubmit={() => {
         onSubmit()
         onClose()
       }}
     >
-      {(props) => (
-        <Form>
-          <Field name='name' validate={onValidate}>
-            {({ field, form }: {field: any, form:any}) => (
+      {(props) => <Form>
+          <Field name="name" validate={onValidate}>
+            {({ field, form }: {field: any, form:any}) => 
               <FormControl isInvalid={form.errors.name && form.touched.name}>
                 <FormLabel>Set Username</FormLabel>
                 <Input {...field} placeholder='test' />
                 <FormErrorMessage>{form.errors.name}</FormErrorMessage>
               </FormControl>
-            )}
+            }
           </Field>
-          <Button mt={4} type="submit" width="full" isLoading={props.isSubmitting}>
+          <Button mt={4} type="submit" width="full"
+isLoading={props.isSubmitting}>
             Submit
           </Button>
-        </Form>
-      )}
+         </Form>}
     </Formik>
   )
 }
